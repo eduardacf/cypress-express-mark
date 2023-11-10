742c04d4f784b1f4a322c2d6086b27b4f45f9ebb
duda <eduarda_26_duda@hotmail.com>
2023-11-10 10:30:03 -0300

    Criando commands

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,62 +2,24 @@
 
 describe('tarefas', () => {
   it('deve cadastrar uma nova tarefa', () => {
-
-    cy.request({
-      url: 'http://localhost:3333/helper/tasks/',
-      method: 'DELETE',
-      body: {
-        name: 'Ler um livro de Node.js'
-      }
-    }).then(response => {
-      expect(response.status).to.eq(204)
-    })
-
-    cy.visit('http://192.168.2.112:8080')
-    cy.get('input[placeholder="Add a new Task"]').type('Ler um livro de Node.js')
-
-    cy.contains('button', 'Create').click()
-
-    cy.contains('main div p', 'Ler um livro de Node.js').should('be.visible')
-
+    const taskName = 'Ler um livro de Node.js'
+    cy.removeTaskByName(taskName)
+    cy.createTask(taskName)
+    cy.contains('main div p', taskName).should('be.visible')
   })
 
   it('não deve permitir tarefa duplicada', () => {
-
-    cy.request({
-      url: 'http://localhost:3333/helper/tasks/',
-      method: 'DELETE',
-      body: {
-        name: 'Estudar Javascript'
-      }
-    }).then(response => {
-      expect(response.status).to.eq(204)
-    })
-
-    //Dado que eu tenho uma tarefa duplicada
-
-    cy.request({
-      url: 'http://localhost:3333/tasks/',
-      method: 'POST',
-      body: {
-        name: 'Estudar Javascript',
-        is_done: false
-      }
-    }).then(response => {
-      expect(response.status).to.eq(201)
-    })
-
-    // Quando faço o cadastro dessa tarefa
-    cy.visit('http://192.168.2.112:8080')
-    cy.get('input[placeholder="Add a new Task"]')
-      .type('Estudar Javascript')
-
-    cy.contains('button', 'Create').click()
-
-    //Então vejo a mensagem de duplicidade
+    const task = {
+      name: 'Estudar Javascript',
+      is_done: false
+    }
+    cy.removeTaskByName(task.name)
+    cy.postTask(task)
+    cy.createTask(task.name)
     cy.get('.swal2-html-container')
       .should('be.visible')
       .should('have.text', 'Task already exists!')
 
   })
 })
+
